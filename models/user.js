1b0e1f8758a5107f4cdfe7243ab292df707976ee
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-10-04 23:51:01 -0300

    Formatting code

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', {
-	id: String,
-	username: String,
-	password: String,
-	email: String,
-	firstName: String,
-	lastName: String,
-	fb: {
-		id: String,
-		access_token: String,
-		firstName: String,
-		lastName: String,
-		email: String
-	},
+  id: String,
+  username: String,
+  password: String,
+  email: String,
+  firstName: String,
+  lastName: String,
+  fb: {
+    id: String,
+    access_token: String,
+    firstName: String,
+    lastName: String,
+    email: String
+  },
 });
